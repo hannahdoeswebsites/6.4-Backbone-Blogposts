e8d5d24d7c214b1fef896dcb4f4b6c7ee2172caa
Hannah Jordan <hjorda0126@aol.com>
2015-06-15 23:14:09 -0400

    all working

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,17 @@
+import View from './view/index';
+import {PostCollection} from './models/post';
+
 (function(){
   'use strict';
 
   $(document).ready(function(){
-    $('body').prepend(JST.application());
+
+  	var posts = new PostCollection();
+
+  	var form = new View({collection: posts});
+
+  	$('#app').html(form.el);
+
   });
+
 })();
---
+++ scripts/view/index.js
@@ -0,0 +1,30 @@
+export default Backbone.View.extend({
+
+	tagName: 'form',
+	className: 'blog-post-form',
+
+	template: JST.index,
+
+	events: {
+		'submit': 'addPost',
+	},
+
+	initialize: function() {
+		this.render();
+	},
+
+	render: function() {
+		this.$el.html(this.template(this.collection.toJSON()));
+	},
+
+	addPost: function(e) {
+		e.preventDefault();
+		var title = this.$('.blog-post-title').val();
+		var content = this.$('.blog-post-content').val();
+		this.collection.create({
+			title: title,
+			content: content
+		});
+	}
+
+});
